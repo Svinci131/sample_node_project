ab5a442428bd3f3c8bac8216f99aa39913df5bd7
Svinci131 <svinci131@gmail.com>
2018-01-31 12:32:41 -0500

    add get tests

--- tests/core/core_api_patient_spec.js
+++ tests/core/core_api_patient_spec.js
@@ -36,9 +36,9 @@ lab.experiment('core/patient controller integration tests', () => {
 
     lab.experiment('List tests', () => {
 
-      //Should return a max number of patients
-      //Should be careful abt sensitive info
-
+      /* Should return a max number of patients */
+      /* Should be careful abt sensitive info */
+      /* Should not include __v or any of that */
       lab.test('Should return an array of all patients.', (done) => {
         Async.waterfall([
           function fakePatients(callback) {
@@ -57,9 +57,9 @@ lab.experiment('core/patient controller integration tests', () => {
             server.inject(req, res => callback(null, res))
           },
           function testPatientsRetrieved(res, callback) {
-            console.log(res.result)
             Code.expect(TestUtils.isRespSuccess(res, 200)).to.be.true()
-            Code.expect(res.result.data.patients.length).to.equal(5)
+            Code.expect(res.result.data.patients.length)
+              .to.equal(5)
 
             return callback()
           }
@@ -68,6 +68,80 @@ lab.experiment('core/patient controller integration tests', () => {
           if (err) throw err
 
           return done()
+        })
+      })
+    })
+
+    lab.experiment('GET tests', () => {
+
+      /* Should return 404 if patient doesn't exist */
+      /* Should return 422 if id is not hex or not there */
+      /* Should not include __v or any of that */
+      /* Should not include virtuals */
+      lab.test('Should return the patient document if found.', (done) => {
+        Async.waterfall([
+          function fakePatients(callback) {
+            FakeFactories.patientFactory.createAndSave(
+              5,
+              null,
+              callback
+            )
+          },
+          function listPatients(patients, callback) {
+            const patient = patients[0]
+            const req = {
+              method: 'GET',
+              url: `/api/v1.0/patients/${patient._id}`
+            }
+
+            server.inject(req, res => callback(null, res, patient))
+          },
+          function testPatientRetrieved(res, _patient, callback) {
+            console.log(res.result)
+            Code.expect(TestUtils.isRespSuccess(res, 200)).to.be.true()
+
+            const patient = res.result.data.patient
+            Code.expect(patient._id).to.equal(_patient._id)
+
+            //TODO FOR LOOP TEST
+            return callback()
+          }
+        ],
+        function finish(err) {
+          if (err) throw err
+
+          return done()
+        })
+      })
+
+      lab.experiment('Error Handling', () => {
+
+        lab.test('Should return 404 on batch doesn\'t exist.', (done) => {
+          const req = {
+            method: 'GET',
+            url: '/api/v1.0/patients/123456789012345678901234'
+          }
+
+          server.inject(req, (res) => {
+            Code.expect(TestUtils.isRespError(res, 404, 'Not Found'))
+              .to.be.true()
+
+            return done()
+          })
+        })
+
+        lab.test('Should return 422 if bad id is given.', (done) => {
+          const req = {
+            method: 'GET',
+            url: '/api/v1.0/patients/AAAA'
+          }
+
+          server.inject(req, (res) => {
+            Code.expect(TestUtils.isRespError(res, 422, 'Invalid Data'))
+              .to.be.true()
+
+            return done()
+          })
         })
       })
 
